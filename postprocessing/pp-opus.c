1c8e46ccd4297b2efa30bde959a05a5c87db4001
Neil Kinnish <hello@neilkinnish.com>
2020-06-28 13:35:35 +0100

    Experiment 3

--- postprocessing/pp-opus.c
+++ postprocessing/pp-opus.c
@@ -82,17 +82,17 @@ int janus_pp_opus_process(FILE *file, janus_pp_frame_packet *list, int *working,
 			int silence_count = (tmp->ts - tmp->prev->ts)/48/20 - 1;
 			pos = (tmp->prev->ts - list->ts) / 48 / 20 + 1;
 			JANUS_LOG(LOG_WARN, "[FILL] pos: %06"SCNu64", writing silences (count=%d)\n", pos, silence_count);
-// 			int i=0;
-// 			for(i=0; i<silence_count; i++) {
-// 				pos = (tmp->prev->ts - list->ts) / 48 / 20 + i + 1;
-// 				if(pos > count) {
-// 					JANUS_LOG(LOG_WARN, "[SKIP] pos: %06" SCNu64 ", skipping silence after position\n", pos);
-// 					break;
-// 				}
+			int i=0;
+			for(i=0; i<silence_count; i++) {
+				pos = (tmp->prev->ts - list->ts) / 48 / 20 + i + 1;				
+				if(pos > count) {
+					JANUS_LOG(LOG_WARN, "[SKIP] pos: %06" SCNu64 ", skipping silence after position\n", pos);
+					break;
+				}
 // 				op->granulepos = 960*(pos); /* FIXME: get this from the toc byte */
 // 				ogg_stream_packetin(stream, op);
 // 				ogg_write();
-// 			}
+			}
 // 			ogg_flush();
 			g_free(op);
 		}
