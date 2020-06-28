2d948757a19f5b14dd6a4404fd4bd11023c5e067
Neil Kinnish <hello@neilkinnish.com>
2020-06-28 13:24:54 +0100

    Experiment log 2

--- postprocessing/pp-opus.c
+++ postprocessing/pp-opus.c
@@ -73,15 +73,15 @@ int janus_pp_opus_process(FILE *file, janus_pp_frame_packet *list, int *working,
 	uint64_t pos = 0;
 	uint8_t *buffer = g_malloc0(1500);
 	while(*working && tmp != NULL) {
-// 		if(tmp->prev != NULL && ((tmp->ts - tmp->prev->ts)/48/20 > 1)) {
-// 			JANUS_LOG(LOG_WARN, "Lost a packet here? (got seq %"SCNu16" after %"SCNu16", time ~%"SCNu64"s)\n",
-// 				tmp->seq, tmp->prev->seq, (tmp->ts-list->ts)/48000);
-// 			/* FIXME Write the silence packet N times to fill in the gaps */
-// 			ogg_packet *op = op_from_pkt((const unsigned char *)opus_silence, sizeof(opus_silence));
-// 			/* use ts differ to insert silence packet */
-// 			int silence_count = (tmp->ts - tmp->prev->ts)/48/20 - 1;
-// 			pos = (tmp->prev->ts - list->ts) / 48 / 20 + 1;
-// 			JANUS_LOG(LOG_WARN, "[FILL] pos: %06"SCNu64", writing silences (count=%d)\n", pos, silence_count);
+		if(tmp->prev != NULL && ((tmp->ts - tmp->prev->ts)/48/20 > 1)) {
+			JANUS_LOG(LOG_WARN, "Lost a packet here? (got seq %"SCNu16" after %"SCNu16", time ~%"SCNu64"s)\n",
+				tmp->seq, tmp->prev->seq, (tmp->ts-list->ts)/48000);
+			/* FIXME Write the silence packet N times to fill in the gaps */
+			ogg_packet *op = op_from_pkt((const unsigned char *)opus_silence, sizeof(opus_silence));
+			/* use ts differ to insert silence packet */
+			int silence_count = (tmp->ts - tmp->prev->ts)/48/20 - 1;
+			pos = (tmp->prev->ts - list->ts) / 48 / 20 + 1;
+			JANUS_LOG(LOG_WARN, "[FILL] pos: %06"SCNu64", writing silences (count=%d)\n", pos, silence_count);
 // 			int i=0;
 // 			for(i=0; i<silence_count; i++) {
 // 				pos = (tmp->prev->ts - list->ts) / 48 / 20 + i + 1;
@@ -94,8 +94,8 @@ int janus_pp_opus_process(FILE *file, janus_pp_frame_packet *list, int *working,
 // 				ogg_write();
 // 			}
 // 			ogg_flush();
-// 			g_free(op);
-// 		}
+			g_free(op);
+		}
 		if(tmp->drop) {
 			/* We marked this packet as one to drop, before */
 			JANUS_LOG(LOG_WARN, "Dropping previously marked audio packet (time ~%"SCNu64"s)\n", (tmp->ts-list->ts)/48000);
