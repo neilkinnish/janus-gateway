28ea002d529d6798e3d2dd030397096b177fbc53
Neil Kinnish <hello@neilkinnish.com>
2020-09-18 09:43:39 +0100

    tweaks

--- postprocessing/pp-utils.c
+++ postprocessing/pp-utils.c
@@ -1,23 +1,23 @@
 #include "pp-utils.h"
 
 void init_dimension_array(dimension_array *arr, size_t initialSize) {
-	arr->array = (struct dimension_item *)malloc(initialSize * sizeof(struct dimension_item));
-	arr->used = 0;
-	arr->size = initialSize;
+    arr->array = (struct dimension_item *)malloc(initialSize * sizeof(struct dimension_item));
+    arr->used = 0;
+    arr->size = initialSize;
 }
 
 void insert_dimension_array(dimension_array *arr, dimension_item element) {
-	if (arr->used == arr->size) {
-		arr->size *= 2;
-		arr->array = (struct dimension_item *)realloc(arr->array, arr->size * sizeof(struct dimension_item));
-	}
-	arr->array[arr->used++] = element;
+    if (arr->used == arr->size) {
+        arr->size *= 2;
+        arr->array = (struct dimension_item *)realloc(arr->array, arr->size * sizeof(struct dimension_item));
+    }
+    arr->array[arr->used++] = element;
 }
 
 void free_dimension_array(dimension_array *arr) {
-	free(arr->array);
-	arr->array = NULL;
-	arr->used = arr->size = 0;
+    free(arr->array);
+    arr->array = NULL;
+    arr->used = arr->size = 0;
 }
 
 dimension_item get_optimal_dimensions(dimension_item arr[], int len, int totalFrames, int threshold) {
